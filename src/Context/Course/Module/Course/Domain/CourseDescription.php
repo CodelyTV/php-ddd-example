9f5f93f8bcf332969a394c92a26169b2cb20da79
Sergio <sergio.decandelario@gmail.com>
2017-10-07 20:18:27 +0200

    Implemented the creation of course

---
+++ src/Context/Course/Module/Course/Domain/CourseDescription.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CodelyTv\Context\Course\Module\Course\Domain;
+
+use CodelyTv\Types\ValueObject\StringValueObject;
+
+/**
+ * CourseDescription
+ */
+final class CourseDescription extends StringValueObject
+{
+
+}
