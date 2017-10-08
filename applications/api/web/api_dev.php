a378fdb5ef1ace60a6b98af35bceb1d8f8f583ad
Sergio <sergio.decandelario@gmail.com>
2017-10-08 18:51:35 +0200

    Added the local ip of the docker container to the api_dev.php

--- applications/api/web/api_dev.php
+++ applications/api/web/api_dev.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(isset($_SERVER, $_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '', ['127.0.0.1', 'fe80::1', '::1'])
+    || !in_array(isset($_SERVER, $_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '', ['127.0.0.1', 'fe80::1', '::1', '172.26.0.1'])
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
