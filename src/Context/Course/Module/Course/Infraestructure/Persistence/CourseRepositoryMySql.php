cc730ed291967ea1ba4688a74ed998dbc95ffa3e
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2017-10-23 20:29:55 +0200

    Modify the concrete repository class

--- src/Context/Course/Module/Course/Infraestructure/Persistence/CourseRepositoryMySql.php
+++ src/Context/Course/Module/Course/Infraestructure/Persistence/CourseRepositoryMySql.php
@@ -5,11 +5,17 @@ namespace CodelyTv\Context\Course\Module\Course\Infraestructure\Persistence;
 use CodelyTv\Context\Course\Module\Course\Domain\Course;
 use CodelyTv\Context\Course\Module\Course\Domain\CourseRepository;
 use CodelyTv\Infrastructure\Doctrine\Repository;
+use CodelyTv\Shared\Domain\CourseId;
 
 final class CourseRepositoryMySql extends Repository implements CourseRepository
 {
     public function save(Course $course): void
     {
         $this->persist($course);
+    }
+
+    public function search(CourseId $id)
+    {
+        return $this->repository(Course::class)->find($id);
     }
 }
