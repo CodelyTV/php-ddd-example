f3e6afe44a32a96dce9e7c9843133e9d2d957ee6
Manel Pérez <yo@manelperez.com>
2018-07-14 17:20:15 +0200

    Removed unused methods

--- src/Context/Video/Module/Video/Infrastructure/Persistence/VideoRepositoryMySql.php
+++ src/Context/Video/Module/Video/Infrastructure/Persistence/VideoRepositoryMySql.php
@@ -7,9 +7,7 @@ namespace CodelyTv\Context\Video\Module\Video\Infrastructure\Persistence;
 use CodelyTv\Context\Video\Module\Video\Domain\Video;
 use CodelyTv\Context\Video\Module\Video\Domain\VideoId;
 use CodelyTv\Context\Video\Module\Video\Domain\VideoRepository;
-use CodelyTv\Context\Video\Module\Video\Domain\Videos;
 use CodelyTv\Shared\Infrastructure\Doctrine\Repository;
-use CodelyTv\Shared\Domain\CourseId;
 
 final class VideoRepositoryMySql extends Repository implements VideoRepository
 {
@@ -21,10 +19,5 @@ final class VideoRepositoryMySql extends Repository implements VideoRepository
     public function search(VideoId $id): ?Video
     {
         return $this->repository(Video::class)->find($id);
-    }
-
-    public function searchByCourseId(CourseId $courseId): Videos
-    {
-        return new Videos($this->repository(Video::class)->findBy(['course_id' => $courseId]));
     }
 }
