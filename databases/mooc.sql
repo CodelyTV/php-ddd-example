92d1ad178496287fa02bea9e8e93c0cdb6b093e6
Javier Ferrer <javier.mailserio@gmail.com>
2019-02-19 20:20:24 +0100

    Support emojis while saving to DB 🤟

--- databases/mooc.sql
+++ databases/mooc.sql
@@ -5,11 +5,11 @@ CREATE TABLE `video` (
   `url` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `course_id` char(36) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `user` (
   `id` char(36) COLLATE utf8_unicode_ci NOT NULL,
   `name` VARCHAR(155) NOT NULL,
   `total_videos_created` INTEGER(5) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8_unicode_ci;
