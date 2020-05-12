dc650c6fbb6c1cf6088e7f842f3c60be091bb5b4
Jacob Dreesen <jacob@hdreesen.de>
2020-05-12 09:34:10 +0200

    Fix typo (#153)

--- src/Shared/Infrastructure/Persistence/Elasticsearch/ElasticQueryGenerator.php
+++ src/Shared/Infrastructure/Persistence/Elasticsearch/ElasticQueryGenerator.php
@@ -28,7 +28,7 @@ final class ElasticQueryGenerator
     public function __invoke(array $query, Filter $filter): array
     {
         $type          = $this->typeFor($filter->operator());
-        $termLevel     = $this->termLeverFor($filter->operator());
+        $termLevel     = $this->termLevelFor($filter->operator());
         $valueTemplate = $filter->operator()->isContaining() ? '*%s*' : '%s';
 
         return array_merge_recursive(
@@ -51,7 +51,7 @@ final class ElasticQueryGenerator
         return in_array($operator->value(), self::$mustNotFields, true) ? self::MUST_NOT_TYPE : self::MUST_TYPE;
     }
 
-    private function termLeverFor(FilterOperator $operator): string
+    private function termLevelFor(FilterOperator $operator): string
     {
         return get($operator->value(), self::$termMapping);
     }
