7eb262f26bb48067f905dc24373e96d32c37e3a2
Rafa Gómez <rgomezcasas@gmail.com>
2020-06-03 15:28:32 +0200

    Move databases to etc

--- src/Mooc/Shared/Infrastructure/Doctrine/MoocEntityManagerFactory.php
+++ src/Mooc/Shared/Infrastructure/Doctrine/MoocEntityManagerFactory.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 
 final class MoocEntityManagerFactory
 {
-    private const SCHEMA_PATH = __DIR__ . '/../../../../../databases/mooc.sql';
+    private const SCHEMA_PATH = __DIR__ . '/../../../../../etc/databases/mooc.sql';
 
     public static function create(array $parameters, string $environment): EntityManagerInterface
     {
