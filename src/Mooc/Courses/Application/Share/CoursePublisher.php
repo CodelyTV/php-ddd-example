60ec9f2a7b865b7b924e15c57c03cf4f1ccd9a6c
gonzalogcontacto <gonzalogcontacto@gmail.com>
2021-02-24 19:32:49 +0100

    * Accesing to course throught course finder

--- src/Mooc/Courses/Application/Share/CoursePublisher.php
+++ src/Mooc/Courses/Application/Share/CoursePublisher.php
@@ -4,17 +4,22 @@ declare(strict_types=1);
 
 namespace CodelyTv\Mooc\Courses\Application\Share;
 
+use CodelyTv\Mooc\Courses\Application\Find\CourseFinder;
 use CodelyTv\Mooc\Courses\Domain\Course;
+use CodelyTv\Mooc\Courses\Domain\CourseRepository;
 use CodelyTv\Mooc\Courses\Domain\ShareRepository;
+use CodelyTv\Mooc\Shared\Domain\Courses\CourseId;
 
 final class CoursePublisher
 {
-    public function __construct(private ShareRepository $repository)
+    public function __construct(private CourseRepository $courseRepository, private ShareRepository $repository)
     {
+        $this->finder = new CourseFinder($courseRepository);
     }
 
-    public function __invoke(Course $course): void
+    public function __invoke(CourseId $courseId): void
     {
+        $course = $this->finder->__invoke($courseId);
         $this->repository->share($course);
     }
 }
