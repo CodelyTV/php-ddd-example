6714113d6f7e2473eda5cf011de84a814667e25a
lucianogarciaz <cluciano.gz@gmail.com>
2021-04-03 10:07:08 +0200

    Renamed to lower case attributes in database

--- etc/databases/openflight.sql
+++ etc/databases/openflight.sql
@@ -7,9 +7,9 @@ USE `open_flight`;
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`
 (
-    `Id`       CHAR(36) NOT NULL,
-    `Name`     TEXT     NOT NULL,
-    `LastName` TEXT     NOT NULL,
-    `Password` TEXT     NOT NULL,
-    PRIMARY KEY (`Id`)
+    `id`       CHAR(36) NOT NULL,
+    `name`     TEXT     NOT NULL,
+    `lastname` TEXT     NOT NULL,
+    `password` TEXT     NOT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
