a16d1593f401b2176a364a21f4a88fa2b77b7efb
Didweb <info@did-web.com>
2021-05-22 20:36:51 +0200

    test

--- src/Mooc/Videos/Application/Find/FindLastVideoQuery.php
+++ src/Mooc/Videos/Application/Find/FindLastVideoQuery.php
@@ -5,7 +5,13 @@ declare(strict_types=1);
 namespace CodelyTv\Mooc\Videos\Application\Find;
 
 
-final class FindLastVideoQuery
+use CodelyTv\Shared\Domain\Bus\Query\Query;
+
+final class FindLastVideoQuery  implements Query
 {
+    public function __construct()
+    {
+    }
+
 
 }
