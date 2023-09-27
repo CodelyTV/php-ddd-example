66a532715bdf5208e1c4ebb81938b9fabcce1237
Rafa Gómez <rgomezcasas@gmail.com>
2023-09-27 14:31:39 +0200

    feat: add easy code standard

---
+++ ecs.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\Strict\StrictParamFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/apps',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $ecsConfig->sets([SetList::PSR_12]);
+
+    $ecsConfig->rules([
+        NoUnusedImportsFixer::class,
+        StrictParamFixer::class
+    ]);
+
+};
