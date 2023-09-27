15c29529672766a0737346835ffa7dced57af5a0
Rafa Gómez <rgomezcasas@gmail.com>
2023-09-27 16:10:29 +0200

    style: add new php cs fixer rules

--- ecs.php
+++ ecs.php
@@ -2,8 +2,56 @@
 
 declare(strict_types=1);
 
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\ArrayNotation\TrimArraySpacesFixer;
+use PhpCsFixer\Fixer\Basic\SingleLineEmptyBodyFixer;
+use PhpCsFixer\Fixer\Casing\ClassReferenceNameCasingFixer;
+use PhpCsFixer\Fixer\Casing\LowercaseStaticReferenceFixer;
+use PhpCsFixer\Fixer\Casing\MagicMethodCasingFixer;
+use PhpCsFixer\Fixer\Casing\NativeFunctionCasingFixer;
+use PhpCsFixer\Fixer\Casing\NativeFunctionTypeDeclarationCasingFixer;
+use PhpCsFixer\Fixer\CastNotation\CastSpacesFixer;
+use PhpCsFixer\Fixer\ClassNotation\FinalClassFixer;
+use PhpCsFixer\Fixer\ClassNotation\FinalPublicMethodForAbstractClassFixer;
+use PhpCsFixer\Fixer\ClassNotation\NoBlankLinesAfterClassOpeningFixer;
+use PhpCsFixer\Fixer\ClassNotation\NoNullPropertyInitializationFixer;
+use PhpCsFixer\Fixer\ClassNotation\NoUnneededFinalMethodFixer;
+use PhpCsFixer\Fixer\ClassNotation\OrderedTypesFixer;
+use PhpCsFixer\Fixer\ClassNotation\ProtectedToPrivateFixer;
+use PhpCsFixer\Fixer\ClassNotation\SelfAccessorFixer;
+use PhpCsFixer\Fixer\ClassNotation\SelfStaticAccessorFixer;
+use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
+use PhpCsFixer\Fixer\ClassUsage\DateTimeImmutableFixer;
+use PhpCsFixer\Fixer\ControlStructure\NoUnneededControlParenthesesFixer;
+use PhpCsFixer\Fixer\ControlStructure\NoUnneededCurlyBracesFixer;
+use PhpCsFixer\Fixer\ControlStructure\NoUselessElseFixer;
+use PhpCsFixer\Fixer\ControlStructure\SimplifiedIfReturnFixer;
+use PhpCsFixer\Fixer\ControlStructure\TrailingCommaInMultilineFixer;
+use PhpCsFixer\Fixer\ControlStructure\YodaStyleFixer;
+use PhpCsFixer\Fixer\Import\FullyQualifiedStrictTypesFixer;
+use PhpCsFixer\Fixer\Import\GlobalNamespaceImportFixer;
+use PhpCsFixer\Fixer\Import\NoLeadingImportSlashFixer;
 use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\Operator\AssignNullCoalescingToCoalesceEqualFixer;
+use PhpCsFixer\Fixer\Operator\NoUselessConcatOperatorFixer;
+use PhpCsFixer\Fixer\Operator\NoUselessNullsafeOperatorFixer;
+use PhpCsFixer\Fixer\Operator\ObjectOperatorWithoutWhitespaceFixer;
+use PhpCsFixer\Fixer\Operator\TernaryToElvisOperatorFixer;
+use PhpCsFixer\Fixer\Operator\TernaryToNullCoalescingFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitConstructFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitDedicateAssertFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitDedicateAssertInternalTypeFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitExpectationFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitMethodCasingFixer;
+use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
+use PhpCsFixer\Fixer\Strict\StrictComparisonFixer;
 use PhpCsFixer\Fixer\Strict\StrictParamFixer;
+use PhpCsFixer\Fixer\StringNotation\SingleQuoteFixer;
+use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayListItemNewlineFixer;
+use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayOpenerAndCloserNewlineFixer;
+use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
+use Symplify\CodingStandard\Fixer\Spacing\MethodChainingNewlineFixer;
+use Symplify\CodingStandard\Fixer\Strict\BlankLineAfterStrictTypesFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
@@ -17,7 +65,66 @@ return function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([SetList::PSR_12]);
 
     $ecsConfig->rules([
+        // Imports
         NoUnusedImportsFixer::class,
-        StrictParamFixer::class
+        FullyQualifiedStrictTypesFixer::class,
+        GlobalNamespaceImportFixer::class,
+        NoLeadingImportSlashFixer::class,
+        // Arrays
+        ArrayListItemNewlineFixer::class,
+        ArrayOpenerAndCloserNewlineFixer::class,
+        TrimArraySpacesFixer::class,
+        // Blank lines
+        BlankLineAfterStrictTypesFixer::class,
+        NoBlankLinesAfterClassOpeningFixer::class,
+        // Spacing
+        MethodChainingNewlineFixer::class,
+        SingleLineEmptyBodyFixer::class,
+        CastSpacesFixer::class,
+        // Casing
+        ClassReferenceNameCasingFixer::class,
+        LowercaseStaticReferenceFixer::class,
+        MagicMethodCasingFixer::class,
+        NativeFunctionCasingFixer::class,
+        NativeFunctionTypeDeclarationCasingFixer::class,
+        // Order
+        OrderedTypesFixer::class,
+        // Architecture
+        FinalClassFixer::class,
+        FinalPublicMethodForAbstractClassFixer::class,
+        ProtectedToPrivateFixer::class,
+        VisibilityRequiredFixer::class,
+        DateTimeImmutableFixer::class,
+        NoUselessElseFixer::class,
+        // Operator
+        AssignNullCoalescingToCoalesceEqualFixer::class,
+        NoUselessConcatOperatorFixer::class,
+        NoUselessNullsafeOperatorFixer::class,
+        ObjectOperatorWithoutWhitespaceFixer::class,
+        TernaryToElvisOperatorFixer::class,
+        TernaryToNullCoalescingFixer::class,
+        // Testing
+        PhpUnitConstructFixer::class,
+        PhpUnitDedicateAssertFixer::class,
+        PhpUnitDedicateAssertInternalTypeFixer::class,
+        PhpUnitExpectationFixer::class,
+        // Other
+        LineLengthFixer::class,
+        NoNullPropertyInitializationFixer::class,
+        NoUnneededFinalMethodFixer::class,
+        SelfAccessorFixer::class,
+        SelfStaticAccessorFixer::class,
+        NoUnneededControlParenthesesFixer::class,
+        NoUnneededCurlyBracesFixer::class,
+        SimplifiedIfReturnFixer::class,
+        TrailingCommaInMultilineFixer::class,
+        DeclareStrictTypesFixer::class,
+        StrictComparisonFixer::class,
+        SingleQuoteFixer::class,
     ]);
+
+    $ecsConfig->ruleWithConfiguration(ArraySyntaxFixer::class, ['syntax' => 'short']);
+    $ecsConfig->ruleWithConfiguration(LineLengthFixer::class, [LineLengthFixer::LINE_LENGTH => 120]);
+    $ecsConfig->ruleWithConfiguration(YodaStyleFixer::class, ['equal' => false, 'identical' => false, 'less_and_greater' => false]);
+    $ecsConfig->ruleWithConfiguration(PhpUnitMethodCasingFixer::class, ['case' => PhpUnitMethodCasingFixer::SNAKE_CASE]);
 };
