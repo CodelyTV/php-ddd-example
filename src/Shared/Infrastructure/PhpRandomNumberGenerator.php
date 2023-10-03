f1e592a2b57f98c80994ac78bd00ebf2c0b6e73a
Rafa Gómez <rgomezcasas@gmail.com>
2023-10-03 22:50:22 +0200

    break: try phpat reporting

--- src/Shared/Infrastructure/PhpRandomNumberGenerator.php
+++ src/Shared/Infrastructure/PhpRandomNumberGenerator.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace CodelyTv\Shared\Infrastructure;
 
+use CodelyTv\Mooc\Videos\Domain\VideoTitle;
 use CodelyTv\Shared\Domain\RandomNumberGenerator;
 
 final class PhpRandomNumberGenerator implements RandomNumberGenerator
 {
     public function generate(): int
     {
+        $uuid = new VideoTitle('test');
+
         return random_int(1, 5);
     }
 }
