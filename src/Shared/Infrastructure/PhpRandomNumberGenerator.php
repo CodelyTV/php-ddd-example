f9bdba37cd385551f8a8060a22ecc6257bbc9d36
Rafa Gómez <rgomezcasas@gmail.com>
2023-10-03 22:50:55 +0200

    Revert "break: try phpat reporting"
    This reverts commit f1e592a2b57f98c80994ac78bd00ebf2c0b6e73a.

--- src/Shared/Infrastructure/PhpRandomNumberGenerator.php
+++ src/Shared/Infrastructure/PhpRandomNumberGenerator.php
@@ -4,15 +4,12 @@ declare(strict_types=1);
 
 namespace CodelyTv\Shared\Infrastructure;
 
-use CodelyTv\Mooc\Videos\Domain\VideoTitle;
 use CodelyTv\Shared\Domain\RandomNumberGenerator;
 
 final class PhpRandomNumberGenerator implements RandomNumberGenerator
 {
     public function generate(): int
     {
-        $uuid = new VideoTitle('test');
-
         return random_int(1, 5);
     }
 }
