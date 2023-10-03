3bb10721b77e53b8c70176e6100e569d299e088f
Rafa Gómez <rgomezcasas@gmail.com>
2023-10-03 22:52:42 +0200

    break: try phpmd reporting

--- src/Shared/Infrastructure/PhpRandomNumberGenerator.php
+++ src/Shared/Infrastructure/PhpRandomNumberGenerator.php
@@ -10,6 +10,20 @@ final class PhpRandomNumberGenerator implements RandomNumberGenerator
 {
     public function generate(): int
     {
-        return random_int(1, 5);
+        $random = random_int(1, 5);
+
+        if ($random > 1) {
+            if ($random > 2) {
+                if ($random > 3) {
+                    if ($random > 4) {
+                        return 5;
+                    } else {
+                        return 4;
+                    }
+                }
+            }
+        }
+
+        return $random;
     }
 }
